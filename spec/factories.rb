64077640dea821f0f92aad58e8faa343a0bab636
Tom Brooks <thomas.brooks@documentsdelivered.com>
2013-08-19 17:30:33 -0700

    Begin user display stuff and/or things.

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Arlen Ca$hbomb"
+    email    "kesharules420@aol.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
